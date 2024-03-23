811e433b486cdcf912fb0ebfdb60e1e95d373f42
ykycha-kolot <55743745+yuriyspz@users.noreply.github.com>
2024-03-23 11:11:06 -0700

    instance profile + pull secret

--- outputs.tf
+++ outputs.tf
@@ -4,8 +4,3 @@ data "aws_caller_identity" "current" {}
 output "caller_user" {
   value = data.aws_caller_identity.current.arn
 }
-
-output "aws_instance_public_ip" {
-  value = module.ec2.aws_instance_public_ip
-  
-}
